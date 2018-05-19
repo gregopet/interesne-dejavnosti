162b12d885c6fc3a3e7b1c17fef18ad3fb516617
Gregor Petrin <gregap@gmail.com>
2018-05-19 21:41:55 +0200

    Add thank you page & automatic logout once parents finish entering data

--- src/main/kotlin/si/francebevk/interesnedejavnosti/MainPage.kt
+++ src/main/kotlin/si/francebevk/interesnedejavnosti/MainPage.kt
@@ -3,6 +3,8 @@ package si.francebevk.interesnedejavnosti
 import ratpack.func.Action
 import ratpack.handling.Chain
 import ratpack.handling.Context
+import ratpack.pac4j.RatpackPac4j
+import ratpack.pac4j.internal.Pac4jAuthenticator
 import si.francebevk.dto.Activity
 import si.francebevk.dto.TimeSlot
 import si.francebevk.ratpack.renderJson
@@ -16,11 +18,18 @@ object MainPage : Action<Chain> {
     override fun execute(t: Chain) = t.route {
         get { html(it) }
         get("activities") { activities(it) }
+        get("finish") { endHtml(it) }
     }
 
 
     private fun html(ctx: Context) {
         ctx.render(Main.template(ctx.user.getAttribute(DbAuthenticator.PUPIL_NAME)  as String, ctx.user.getAttribute(DbAuthenticator.PUPIL_CLASS) as String))
+    }
+
+    private fun endHtml(ctx: Context) {
+        RatpackPac4j.logout(ctx).then {
+            ctx.render(Finish.template())
+        }
     }
 
     private fun activities(ctx: Context) {
