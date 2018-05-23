2c35ada9e9194e64969cffe169f21885451a8438
Gregor Petrin <gregap@gmail.com>
2018-05-23 19:35:59 +0200

    Add switch to easily turn off email sending during development

--- src/main/kotlin/si/francebevk/interesnedejavnosti/EmailDispatch.kt
+++ src/main/kotlin/si/francebevk/interesnedejavnosti/EmailDispatch.kt
@@ -10,31 +10,37 @@ object EmailDispatch {
 
     val LOG = LoggerFactory.getLogger(EmailDispatch::class.java)
 
+    const val skipEmails = false
+
     /**
      * Sends the invitation mail with the access code.
      */
     fun sendWelcomeEmail(to: String, pupilName: String, pupilClass: String, accessCode: String, config: EmailConfig) {
-        LOG.info("Sending welcome email to $to")
-        val message = config.startNewMessage()
-        message.subject = "OŠ Franceta Bevka: prijava interesnih dejavnosti za učenca/učenko $pupilName"
-        message.addTo(to)
-        message.setHtmlMsg(WelcomeMailHtml.template(pupilName, pupilClass, accessCode).render().toString())
-        message.setTextMsg(WelcomeMailPlain.template(pupilName, pupilClass, accessCode).render().toString())
-        message.send()
+        if (!skipEmails) {
+            LOG.info("Sending welcome email to $to")
+            val message = config.startNewMessage()
+            message.subject = "OŠ Franceta Bevka: prijava interesnih dejavnosti za učenca/učenko $pupilName"
+            message.addTo(to)
+            message.setHtmlMsg(WelcomeMailHtml.template(pupilName, pupilClass, accessCode).render().toString())
+            message.setTextMsg(WelcomeMailPlain.template(pupilName, pupilClass, accessCode).render().toString())
+            message.send()
+        }
     }
 
     /**
      * Sends the confirmation mail once people have finished editing the page.
      */
     fun sendConfirmationMail(to: String, pupilName: String, pupilClass: String, leaveTimes: PupilSettings, activities: List<Activity>, config: EmailConfig) {
-        LOG.info("Sending confirmation email to $to")
-        val message = config.startNewMessage()
-        message.subject = "OŠ Franceta Bevka: uspešna prijava interesnih dejavnosti za učenca/učenko $pupilName"
-        message.addTo(to)
-        message.setTextMsg(
-            ConfirmationMailPlain.template(pupilName, pupilClass, leaveTimes, activities).render().toString()
-        )
-        message.send()
+        if (!skipEmails) {
+            LOG.info("Sending confirmation email to $to")
+            val message = config.startNewMessage()
+            message.subject = "OŠ Franceta Bevka: uspešna prijava interesnih dejavnosti za učenca/učenko $pupilName"
+            message.addTo(to)
+            message.setTextMsg(
+                    ConfirmationMailPlain.template(pupilName, pupilClass, leaveTimes, activities).render().toString()
+            )
+            message.send()
+        }
     }
 
     private fun EmailConfig.startNewMessage() = HtmlEmail().also {
