95605565d18560d14db77ce808dc95a01c4593f2
Gregor Petrin <gregap@gmail.com>
2018-05-23 19:46:17 +0200

    Add DAO methods for storing leave times & extended stay flag

--- src/main/kotlin/si/francebevk/interesnedejavnosti/PupilDAO.kt
+++ src/main/kotlin/si/francebevk/interesnedejavnosti/PupilDAO.kt
@@ -18,4 +18,36 @@ object PupilDAO {
         .where(ACCESS_CODE.eq(code))
         .fetchOne()
     }
+
+    /**
+     * Indicates that the pupil will be involved in after school stay, at specified times.
+     */
+    fun storeLeaveTimes(mon: Short?, tue: Short?, wed: Short?, thu: Short?, fri: Short?, pupilId: Long, jooq: DSLContext) = with(PUPIL) {
+        jooq
+        .update(PUPIL)
+        .set(LEAVE_MON, mon)
+        .set(LEAVE_TUE, tue)
+        .set(LEAVE_WED, wed)
+        .set(LEAVE_THU, thu)
+        .set(LEAVE_FRI, fri)
+        .set(EXTENDED_STAY, true)
+        .where(ID.eq(pupilId))
+        .execute()
+    }
+
+    /**
+     * Indicates that the pupil will not be involved in after school stay.
+     */
+    fun storeNonParticipation(pupilId: Long, jooq: DSLContext) = with(PUPIL) {
+        jooq
+        .update(PUPIL)
+        .set(LEAVE_MON, null as Short?)
+        .set(LEAVE_TUE, null as Short?)
+        .set(LEAVE_WED, null as Short?)
+        .set(LEAVE_THU, null as Short?)
+        .set(LEAVE_FRI, null as Short?)
+        .set(EXTENDED_STAY, false)
+        .where(ID.eq(pupilId))
+        .execute()
+    }
 }
